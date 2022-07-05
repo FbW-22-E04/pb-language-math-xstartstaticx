17963acae1b58baca7f8e5e5dabaa61df17db74b
xstartstaticx <daniel.reiller@icloud.com>
2022-07-05 14:58:41 +0200

    finished exercise

---
+++ solution.js
@@ -0,0 +1,21 @@
+// 1 Min & Max
+console.log("1.a", Math.min(-1, 0, 1, 2, 3, 4));
+console.log("1.b", Math.max(-1, 0, 1, 2, 3, 4));
+
+// 2 Rounding
+console.log("2.a", Math.ceil(3321.32321));
+console.log("2.a", Math.ceil(326.76));
+console.log("2.a", Math.ceil(76788.7));
+console.log("2.a", Math.ceil(-9.78));
+console.log("2.a", Math.ceil(43.342));
+
+console.log("2.b", Math.floor(3321.32321));
+console.log("2.b", Math.floor(326.76));
+console.log("2.b", Math.floor(76788.7));
+console.log("2.b", Math.floor(-9.78));
+console.log("2.b", Math.floor(43.342));
+
+// 3 Dice Roll
+// const diceRoll = Math.ceil(Math.random() * 6);
+// console.log(diceRoll);
+console.log(Math.ceil(Math.random() * 6));
